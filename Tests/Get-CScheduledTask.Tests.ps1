949d203a68d42d2fbe5c3655506b3556eae99219
Aaron Jensen <ajensen@webmd.net>
2024-03-26 14:36:04 -0700

    Migrating tests to Pester 5.

--- Tests/Get-CScheduledTask.Tests.ps1
+++ Tests/Get-CScheduledTask.Tests.ps1
@@ -1,22 +1,14 @@
 
 using module ..\Carbon.ScheduledTasks;
 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#Requires -Version 5.1
+Set-StrictMode -Version 'Latest'
 
+BeforeAll {
+    Set-StrictMode -Version 'Latest'
 
-& (Join-Path -Path $PSScriptRoot -ChildPath 'Initialize-Test.ps1' -Resolve)
+    & (Join-Path -Path $PSScriptRoot -ChildPath 'Initialize-Test.ps1' -Resolve)
 
-Describe 'Get-CScheduledTask' {
     function Assert-ScheduledTaskEqual
     {
         param(
@@ -55,7 +47,6 @@ Describe 'Get-CScheduledTask' {
             $propertyName = $columnName -replace '[^A-Za-z0-9_]',''
 
             Write-Debug ('  {0} <=> {1}' -f $propertyName,$columnName)
-            $failMsg = '{0}; column {1}; property {2}' -f $Actual.FullName,$columnName,$propertyName
             if( $propertyName -eq 'TaskName' )
             {
                 $name = Split-Path -Leaf -Path $Expected.TaskName
@@ -64,8 +55,17 @@ Describe 'Get-CScheduledTask' {
                 {
                     $path = '{0}\' -f $path
                 }
-                $Actual.TaskName | Should -Be $name -Because ('{0}  TaskName' -f $task.FullName)
-                $Actual.TaskPath | Should -Be $path -Because ('{0}  TaskPath' -f $task.FullName)
+                $displayName = ''
+                if ($task | Get-Member -Name 'FullName')
+                {
+                    $displayName = $task.FullName
+                }
+                elseif ($task | Get-Member -Name 'RegistrationInfo')
+                {
+                    $displayName = $task.RegistrationInfo.URI
+                }
+                $Actual.TaskName | Should -Be $name -Because ("${displayName}  TaskName")
+                $Actual.TaskPath | Should -Be $path -Because ("${displayName}  TaskPath")
             }
             elseif ($propertyName -in @( 'NextRunTime', 'LastRuntime' ) -and `
                     (
@@ -119,15 +119,16 @@ Describe 'Get-CScheduledTask' {
                 ($Actual.$propertyName) | Should -Be $expectedValue -Because $because
             }
         }
-
-
     }
+}
 
+
+Describe 'Get-CScheduledTask' {
     BeforeEach {
         $Global:Error.Clear()
     }
 
-    It 'should get each scheduled task' {
+    It 'gets each scheduled task' {
         schtasks /query /v /fo csv |
             ConvertFrom-Csv |
             Where-Object { $_.TaskName -and $_.HostName -ne 'HostName' } |
@@ -135,34 +136,17 @@ Describe 'Get-CScheduledTask' {
             ForEach-Object {
                 $expectedTask = $_
                 $task = Get-CScheduledTask -Name $expectedTask.TaskName
-                $task | Should Not BeNullOrEmpty
+                $task | Should -Not -BeNullOrEmpty
 
                 Assert-ScheduledTaskEqual $expectedTask $task
             }
     }
 
-    It 'should get schedules' {
+    It 'gets schedules' {
         $multiScheduleTasks = Get-CScheduledTask | Where-Object { $_.Schedules.Count -gt 1 }
 
-        $multiScheduleTasks | Should Not BeNullOrEmpty
+        $multiScheduleTasks | Should -Not -BeNullOrEmpty
 
-        $taskProps = @(
-                            'HostName',
-                            'TaskName',
-                            'Next Run Time',
-                            'Status',
-                            'Logon Mode',
-                            'Last Run Time',
-                            'Author',
-                            'Task To Run',
-                            'Start In',
-                            'Comment',
-                            'Scheduled Task State',
-                            'Idle Time',
-                            'Power Management',
-                            'Run As User',
-                            'Delete Task If Not Rescheduled'
-                     )
         foreach( $multiScheduleTask in $multiScheduleTasks )
         {
             $expectedSchedules = schtasks /query /v /fo csv /tn $multiScheduleTask.FullName | ConvertFrom-Csv
@@ -175,36 +159,26 @@ Describe 'Get-CScheduledTask' {
         }
     }
 
-    It 'should support wildcards' {
+    It 'supports wildcards for name' {
         $expectedTask = Get-CScheduledTask -AsComObject | Select-Object -First 1
         $expectedTask | Should -Not -BeNullOrEmpty
         $wildcard = ('*{0}*' -f $expectedTask.Path.Substring(1,$expectedTask.Path.Length - 2))
         $task = Get-CScheduledTask -Name $wildcard
         $task | Should -Not -BeNullOrEmpty
         $task.GetType().FullName | Should -Be ([Carbon_ScheduledTasks_TaskInfo].FullName)
-        Join-Path -Path $task.TaskPath -ChildPath $task.TaskName | Should Be $expectedTask.Path
+        Join-Path -Path $task.TaskPath -ChildPath $task.TaskName | Should -Be $expectedTask.Path
     }
-}
 
-Describe 'Get-CScheduledTask.when getting all tasks' {
-    It 'should get all scheduled tasks' {
+    It 'gets all scheduled tasks' {
         $expectedTasks = Get-CScheduledTask -AsComObject | Measure-Object
         $actualTasks = Get-CScheduledTask
         $actualTasks.Count | Should -Be $expectedTasks.Count
     }
 
-}
-
-Describe 'Get-CScheduledTask.when task does not exist' {
-    $Global:Error.Clear()
-    $result = Get-CScheduledTask -Name 'fjdskfjsdflkjdskfjsdklfjskadljfksdljfklsdjf' -ErrorAction SilentlyContinue
-    It 'write no errors' {
-        $Global:Error.Count | Should BeGreaterThan 0
-        $Global:Error[0] | Should Match 'not found'
-    }
-
-    It 'should return nothing' {
-        $result | Should BeNullOrEmpty
+    It 'ignores non-existent task' {
+        $result = Get-CScheduledTask -Name 'fjdskfjsdflkjdskfjsdklfjskadljfksdljfklsdjf' -ErrorAction SilentlyContinue
+        $Global:Error.Count | Should -BeGreaterThan 0
+        $Global:Error[0] | Should -Match 'not found'
+        $result | Should -BeNullOrEmpty
     }
 }
-
